75f6d93ae8cae1ede39ce0eb172f2a15f59ee3d6
Martin Fencl <martin.fencl@gmail.com>
2016-06-25 17:04:23 -0500

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,12 @@ class VisitorsController < ApplicationController
   def new
     # Rails.logger.debug 'DEBUG :: entering new method'
     @owner = Owner.new
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
     # logger.debug 'DEBUG :: Owner name is ' + @owner.name # <= works without using Rails.logger ...
     # raise 'Deliberate Failure'
+    # render 'visitors/new', :layout => false
+    # render 'visitors/new', :layout => 'special'
     # render 'visitors/new' # <= Rails does automatically = the magick ... know rails conventions !!!!!!
   end
 
